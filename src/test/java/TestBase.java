b0e32b4d729d4e26f258596eea391edfb90a8d73
FridmanS <86373775+FridmanS@users.noreply.github.com>
2021-07-13 18:14:58 +0300

    Create TestBase.java

---
+++ src/test/java/TestBase.java
@@ -0,0 +1,41 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+
+    WebDriver wd;
+
+    @BeforeClass
+    public void init(){
+        wd = new ChromeDriver();
+        wd.manage().window().maximize();
+        wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        wd.navigate().to("https://contacts-app.tobbymarshall815.vercel.app/home");
+    }
+
+    public void click(By locator){
+        wd.findElement(locator).click();
+    }
+
+    public void type(By locator, String text){
+        WebElement element = wd.findElement(locator);
+        click(locator);
+        element.clear();
+        element.sendKeys(text);
+    }
+
+    public String getText(By locator){
+        return wd.findElement(locator).getText();
+    }
+
+    @AfterClass
+    public void tearDown(){
+        wd.quit();
+    }
+}
