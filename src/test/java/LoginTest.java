abf35c159fe77e7a21504c2d6bf8634f2105d98b
FridmanS <86373775+FridmanS@users.noreply.github.com>
2021-08-03 20:39:03 +0300

    Logger added

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -18,15 +18,19 @@ public class LoginTest extends TestBase {
         app.userHelper().fillLoginForm("12345@asd.com", "aA1234567$");
         app.userHelper().submitLogin();
         Assert.assertEquals(app.userHelper().getTextOfCheckElement(), "Sign Out");
+        logger.info("Test passed");
     }
 
     @Test
     public void loginTestWithWrongPassword(){
         User user = new User().withEmail("12345@asd.com").withPassword("aA1234567");
+
         app.userHelper().openLoginForm();
         app.userHelper().fillLoginForm(user);
+        logger.info(String.format("Login with Email: %s; Password: %s;", user.getEmail(), user.getPassword()));
         app.userHelper().submitLogin();
         app.userHelper().acceptAlert();
         Assert.assertFalse(app.userHelper().isLogged());
+        logger.info("Test passed");
     }
 }
--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,15 +1,27 @@
 import application.ApplicationManager;
 import org.openqa.selenium.remote.BrowserType;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeSuite;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.*;
 
+import java.lang.reflect.Method;
 import java.util.concurrent.TimeUnit;
 
 public class TestBase {
 
     protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
+
+    @BeforeMethod
+    public void startLogger(Method m){
+        logger.info("Start test " + m.getName());
+    }
+
+    @AfterMethod
+    public void endLogger(Method m){
+        logger.info("End of test " + m.getName());
+    }
 
     @BeforeSuite(alwaysRun = true)
     public void setUp(){
